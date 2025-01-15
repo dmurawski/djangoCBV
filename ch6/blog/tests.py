73db6fd5d52143bb6a074b070c0688f372134b8d
Damian Murawski <dmurawski018@gmail.com>
2025-01-15 16:33:10 +0100

    Chapter 7: Forms

--- ch6/blog/tests.py
+++ ch6/blog/tests.py
@@ -28,14 +28,14 @@ class BlogTests(TestCase):
         # test def __str__
         self.assertEqual(str(self.post), "test title")
         # test def get_absolute_url
-        self.assertEqual(self.post.get_absolute_url(), "/post/1")
+        self.assertEqual(self.post.get_absolute_url(), "/post/1/")
 
     def test_url_exists_at_correct_location_listview(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
     def test_url_exists_at_correct_location_detailview(self):
-        response = self.client.get("/post/1")
+        response = self.client.get("/post/1/")
         self.assertEqual(response.status_code, 200)
 
     def test_post_listview(self):
@@ -48,8 +48,37 @@ class BlogTests(TestCase):
         response = self.client.get(
             reverse("post-detail", kwargs={"pk": self.post.pk}),
         )
-        no_response = self.client.get("/post/1111")
+        no_response = self.client.get("/post/1111/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "test title")
         self.assertTemplateUsed(response, "post_detail.html")
+
+    def test_post_createview(self):
+        response = self.client.post(
+            reverse("post-new"),
+            {
+                "title": "New title",
+                "body": "Test body",
+                "author": self.user.id,
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "New title")
+        self.assertEqual(Post.objects.last().body, "Test body")
+
+    def test_post_updateview(self):
+        response = self.client.post(
+            reverse("post-edit", args="1"),
+            {
+                "title": "New title edit",
+                "body": "Test body edit",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "New title edit")
+        self.assertEqual(Post.objects.last().body, "Test body edit")
+
+    def test_post_deleteview(self):
+        response = self.client.post(reverse("post-delete", args="1"))
+        self.assertEqual(response.status_code, 302)
--- ch6/blog/urls.py
+++ ch6/blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.post_list, name="home"),
-    path("post/<int:pk>", views.post_detail, name="post-detail"),
+    path("", views.BlogListView.as_view(), name="home"),
+    path("post/<int:pk>/", views.BlogDetailView.as_view(), name="post-detail"),
+    path("post/new/", views.BlogCreateView.as_view(), name="post-new"),
+    path("post/<int:pk>/edit/", views.BlogUpdateView.as_view(), name="post-edit"),
+    path("post/<int:pk>/delete/", views.BlogDeleteView.as_view(), name="post-delete"),
 ]
