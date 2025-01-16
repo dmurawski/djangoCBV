710012bb74f49936ac1509833462ba58f7946ddf
Damian Murawski <dmurawski018@gmail.com>
2025-01-16 20:19:23 +0100

    add login mixin

--- ch9-newspaper-project/articles/views.py
+++ ch9-newspaper-project/articles/views.py
@@ -1,33 +1,34 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Article
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = "articles/article_list.html"
     context_object_name = "articles"
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = "articles/article_detail.html"
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     model = Article
     fields = ("title", "body")
     template_name = "articles/article_edit.html"
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = Article
     template_name = "articles/article_delete.html"
     success_url = reverse_lazy("article-list")
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = "articles/article_new.html"
     fields = ("title", "body")
